ec2e6342d82ae2983630f17a4380b14777ac7d9f
David-EU-PL <dawidplatek.eu.pl@outlook.com>
2023-05-03 17:41:46 +0200

    Add project files

---
+++ Calculator/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            { 
+                Console.WriteLine("Podaj 1. liczbę");
+                var number1 = GetInPut();
+                
+                Console.WriteLine("Jaką operację chcesz wykonać? Możliwe operacje to: '+;, '-', '/', '*';");
+                var action = Console.ReadLine();
+
+                Console.WriteLine("Podaj 2. liczbę");
+                var number2 = GetInPut();
+
+                var result = Calculate(number1, number2, action);
+
+                Console.WriteLine("Wynik Twojego działania to: " + result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    
+        private static int GetInPut()
+        {
+            if (!int.TryParse(Console.ReadLine(), out int input))
+                throw new Exception("Podana wartość jest nieprawidłowa");
+
+            return input;
+        }
+    private static int Calculate(int number1, int number2, string action) 
+        {
+            switch (action)
+            {
+                case "+":
+                    return number1 + number2;
+                case "-":
+                    return number1 - number2;
+                case "/":
+                    return number1 / number2;
+                case "*":
+                    return number1 * number2;
+                default:
+                    throw new Exception("Wybrałeś złą operację");
+            }
+        }
+    }
+}
+
+
+    
